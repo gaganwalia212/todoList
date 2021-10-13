e7fea1bae9ea1c52912ec6c46d62e72da35b9e87
Gagan <gaganwalia212@gmail.com>
2021-10-13 23:36:49 +0530

    add items in the todo list

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ app.use(bodyParesr.urlencoded({extended:true}))
 app.listen(3000,function(){
 	console.log("Server is listening")
 })
-
+var task=[]
 app.get("/",function(req,res){
 	var options = {  year: 'numeric', month: 'long', day: 'numeric',weekday: 'long' };
 	var today  = new Date();
 	var hindiDay=today.toLocaleDateString("en-US",options)
 	var englishDay=today.toLocaleDateString("hi-IN",options)
-	res.render('index',{hindiDay:hindiDay,englishDay:englishDay})
+	res.render('index',{hindiDay:hindiDay,englishDay:englishDay,newItem:task})
 	
 })
 
 app.post("/",function(req,res){
-	var task=req.body.task
+	task.push(req.body.task)
 	console.log(task)
 	res.redirect("/")
 })
