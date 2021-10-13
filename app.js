63d394dcca3fe8e286cbaa9a0fba57ce24b7d2fa
Gagan <gaganwalia212@gmail.com>
2021-10-13 19:25:51 +0530

    todo list made

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express=require('express')
 const bodyParesr=require('body-parser')
+
 app=express()
+
 app.set('view engine','ejs')
 app.use(bodyParesr.urlencoded({extended:true}))
+
 
 app.listen(3000,function(){
 	console.log("Server is listening")
 })
 
 app.get("/",function(req,res){
-	res.sendFile(__dirname+"/index.html")
+	var options = {  year: 'numeric', month: 'long', day: 'numeric',weekday: 'long' };
+	var today  = new Date();
+	var hindiDay=today.toLocaleDateString("en-US",options)
+	var englishDay=today.toLocaleDateString("hi-IN",options)
+	res.render('index',{hindiDay:hindiDay,englishDay:englishDay})
 	
 })
 
 app.post("/",function(req,res){
-	var n1=req.body.n1
-	var n2=req.body.n2
-	var result=Number(n1)+Number(n2)
-	res.render("index",{result:result,n1:n1,n2:n2})
-
+	var task=req.body.task
+	console.log(task)
+	res.redirect("/")
 })
